18b45a60fed70da492921175f0660c9964ef92d3
fraberret <f.berretta02@gmail.com>
2024-04-08 18:53:22 +0200

    add featured products card data in data.js const

--- src/data.js
+++ src/data.js
@@ -26,3 +26,38 @@ export const navVoices = [
 
 ]
 
+
+export const featuredProducts = [
+    {
+        name: 'Black Leather Jacket',
+        image: '/images/black_elegant_leather_jacket-400x520.jpg',
+        tags: 'Men, Jackets, Jeans',
+        realPrice: 235,
+        discountedPrice: 200
+    },
+    {
+        name: 'Black Leather Suit',
+        image: '/images/black_leather_suit-400x520.jpg',
+        tags: 'Men, Jackets',
+        realPrice: 176,
+        discountedPrice: false
+    },
+    {
+        name: 'Blue Jacket & Stripe Tee',
+        image: '/images/blue_jacket_and_white_stripe_tee-400x520.jpg',
+        tags: 'Men, Jackets, Suits',
+        realPrice: 580,
+        discountedPrice: false
+    },
+    {
+        name: 'Modern Black Leather Suit',
+        image: '/images/modern_black_leather_suit-400x520.jpg',
+        tags: 'Men, Jackets',
+        realPrice: 96,
+        discountedPrice: false
+    },
+
+
+
+]
+
