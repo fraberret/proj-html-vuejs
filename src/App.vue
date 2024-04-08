691d8710db500ea254b618dc45ba81c3ea311ad7
fraberret <f.berretta02@gmail.com>
2024-04-08 17:35:01 +0200

    add image and text on jumbotron

--- src/App.vue
+++ src/App.vue
@@ -25,14 +25,43 @@ export default {
 
     <main>
 
+        <div class="jumbotron text-center text-white">
+
+
+            <img src="/public/images/home1_slide_three_bg_2.jpg" alt="">
+
+            <div class="jumbotron_text">
+                <h1 class="m-0">Brand New Arrivals</h1>
+                <p>NEW COLLECTION FROM NEW YORK</p>
+            </div>
+
+        </div>
+
     </main>
 </template>
 
 
 
 <style lang="scss" scoped>
-.cart {
-    padding-left: 5rem;
-    padding-right: 5rem;
+.jumbotron {
+    position: relative;
+}
+
+img {
+    width: 100%;
+    object-fit: cover;
+
+}
+
+.jumbotron_text {
+    margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    & h1 {
+        font-size: 90px;
+    }
 }
 </style>
