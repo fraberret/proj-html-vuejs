0ef1581891bf40b191e6ee20f11858e447ceda36
fraberret <f.berretta02@gmail.com>
2024-04-08 17:05:53 +0200

    create a component for Nav and pass data by props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,13 @@
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import NavBar from './NavBar.vue';
+
 
 export default {
     name: 'AppHeader',
+    components: {
+        NavBar
+    },
     props: [
         "navVoices"
     ]
@@ -61,22 +66,7 @@ export default {
             <img src="/public/images/classic_shop_logo1x.png" alt="">
 
             <!-- Bottom nav -->
-            <nav class="d-flex align-items-center">
-                <ul class="d-flex gap-4 m-0">
-
-                    <!-- Nav voices -->
-                    <li v-for="navVoice in navVoices">{{ navVoice.name }}
-                        <font-awesome-icon v-if="navVoice.chevronDown == true" :icon="['fas', 'chevron-down']" />
-
-                    </li>
-
-                </ul>
-
-                <button class="bg_accent_light btn text-light rounded-pill py-2 px-3 my-3 mx-4">SHOP NOW!</button>
-
-                <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-
-            </nav>
+            <NavBar :navVoices />
 
 
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'NavBar',
+    props: [
+        "navVoices"
+    ]
+}
+</script>
+
+<template>
+    <nav class="d-flex align-items-center">
+        <ul class="d-flex gap-4 m-0">
+
+            <!-- Nav voices -->
+            <li v-for="navVoice in navVoices">{{ navVoice.name }}
+                <font-awesome-icon v-if="navVoice.chevronDown == true" :icon="['fas', 'chevron-down']" />
+
+            </li>
+
+        </ul>
+
+        <button class="bg_accent_light btn text-light rounded-pill py-2 px-3 my-3 mx-4">SHOP NOW!</button>
+
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+
+    </nav>
+</template>
+
+
+
+<style lang="scss" scoped></style>
