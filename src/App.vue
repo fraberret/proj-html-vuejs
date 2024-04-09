bde2d520b115d15c175b24f9fee66924d6860f44
fraberret <f.berretta02@gmail.com>
2024-04-09 13:46:39 +0200

    add featured sections (structure)

--- src/App.vue
+++ src/App.vue
@@ -97,12 +97,12 @@ export default {
                                     <!-- real price -->
                                     <span :class="{ 'real_price': product.discountedPrice }" class="pe-2">${{
                                         product.realPrice
-                                    }}</span>
+                                        }}</span>
 
                                     <!--discounted price  -->
                                     <span v-if="product.discountedPrice != false" class="discount_price pe-2">${{
                                         product.discountedPrice
-                                    }}</span>
+                                        }}</span>
                                 </div>
 
                             </div>
@@ -320,7 +320,7 @@ export default {
         <!-- Blog section -->
         <div class="blog_section text-center my-5">
 
-            <div class="title_section">
+            <div class="container title_section">
 
                 <!-- Title -->
                 <div class="title d-flex w-100 justify-content-between align-items-center">
@@ -370,6 +370,73 @@ export default {
                 </div>
             </div>
         </div>
+
+        <!-- Line -->
+        <div class="line w-100 border"></div>
+
+        <!-- Featured section -->
+        <div class="featured_section container my-5">
+            <div class="row">
+
+                <!-- Featured -->
+                <div class="col-3 flex-column">
+                    <h5 class="my-3">FEATURED</h5>
+                    <div class="col-4 d-flex border-bottom justify-content-between w-100 pt-3"
+                        v-for="product in featuredProducts">
+                        <div>
+                            <p>{{ product.name }}</p>
+                            <p>${{ product.realPrice }}</p>
+                        </div>
+
+                        <img class="pb-4" :src="product.image" alt="">
+                    </div>
+                </div>
+
+                <!-- On sale -->
+                <div class="col-3 flex-column">
+                    <h5 class="my-3">ON SALE</h5>
+                    <div class="col-4 d-flex border-bottom justify-content-between w-100 pt-3"
+                        v-for="product in featuredProducts">
+                        <div>
+                            <p>{{ product.name }}</p>
+                            <p>${{ product.realPrice }}</p>
+                        </div>
+
+                        <img class="pb-4" :src="product.image" alt="">
+                    </div>
+                </div>
+
+                <!-- Top rated -->
+                <div class="col-3 flex-column">
+                    <h5 class="my-3">TOP RATED</h5>
+                    <div class="col-4 d-flex border-bottom justify-content-between w-100 pt-3"
+                        v-for="product in featuredProducts">
+                        <div>
+                            <p>{{ product.name }}</p>
+                            <p>${{ product.realPrice }}</p>
+                        </div>
+
+                        <img class="pb-4" :src="product.image" alt="">
+                    </div>
+                </div>
+
+                <!-- Latest review -->
+                <div class="col-3 flex-column">
+                    <h5 class="my-3">LATEST REVIEW</h5>
+                    <div class="col-4 d-flex border-bottom justify-content-between w-100 pt-3"
+                        v-for="product in featuredProducts">
+                        <div>
+                            <p>{{ product.name }}</p>
+                            <p>${{ product.realPrice }}</p>
+                        </div>
+
+                        <img class="pb-4" :src="product.image" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 
 
     </main>
@@ -426,6 +493,12 @@ img {
 
     & img {
         width: 5%;
+    }
+}
+
+.featured_section {
+    & img {
+        width: 20%;
     }
 }
 </style>
