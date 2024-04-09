df04f4a3f38567eb5dab57492f25baf905fc78b9
fraberret <f.berretta02@gmail.com>
2024-04-09 17:14:14 +0200

    create appFooter component and import in app

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import { navVoices } from './data.js'
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import Socials from './components/Socials.vue'
+import AppFooter from './components/AppFooter.vue';
 
 
 
@@ -13,6 +14,7 @@ export default {
     components: {
         AppHeader,
         AppMain,
+        AppFooter,
         Socials
     },
 
@@ -32,113 +34,11 @@ export default {
 
     <AppMain :featuredProducts />
 
-    <footer>
-
-        <!-- Newsletter section -->
-        <div class="subscribe bg_dark text-white py-5">
-            <div class=" py-5 container d-flex justify-content-between align-items-center">
-                <font-awesome-icon :icon="['fas', 'envelope']" />
-                <h4 class="m-0">Lorem ipsum dolor sit amet consectetur.</h4>
-                <div>
-                    <input type="text" class="py-2 px-4 rounded-pill" placeholder="Insert your email">
-                    <button class="bg_accent_light btn text-light rounded-pill py-2 px-5  ms-2">SEND</button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Main footer section -->
-        <div class="text-white bg-black">
-            <div class="featured_section container py-5  ">
-                <div class="row">
-
-                    <!-- Featured -->
-                    <div class="col-3 flex-column">
-                        <img src="/images/classic_shop_logo_footer.png" width="100" alt="">
-                        <div class="my-5 body">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <Socials />
-                    </div>
+    <AppFooter :featuredProducts />
 
 
-
-                    <!-- Top rated -->
-                    <div class="col-3 flex-column">
-                        <h5 class="my-3">TOP RATED</h5>
-                        <div class="col-4 d-flex justify-content-between w-100 pt-3"
-                            v-for="product in featuredProducts">
-                            <div>
-                                <p>{{ product.name }}</p>
-                                <p>${{ product.realPrice }}</p>
-                            </div>
-
-                            <img class="pb-4" :src="product.image" alt="">
-                        </div>
-                    </div>
-
-                    <!-- Posts  -->
-                    <div class="col-3 flex-column">
-                        <h5 class="my-3">RECENT POSTS</h5>
-                        <div class="col-4 d-flex justify-content-between w-100 pt-3">
-                            <div>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                    <!-- Tags -->
-                    <div class="col-3 flex-column">
-                        <h5 class="my-3">TAGS</h5>
-                        <div class="col-4 d-flex justify-content-between w-100 pt-3">
-                            <div>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <p>Lorem ipsum dolor sit amet.</p>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- COpyright section -->
-        <div class="bg-black text-center text-white py-5">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, repudiandae.</p>
-            <img src="/images/payment_cards_footer.png" alt="">
-        </div>
-
-
-    </footer>
 </template>
 
 
 
-<style lang="scss" scoped>
-.logo {
-    color: white;
-}
-
-.featured_section {
-    & img {
-        width: 20%;
-    }
-}
-</style>
+<style lang="scss" scoped></style>
