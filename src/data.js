102c97554047b667001411f0801b8837e2acf603
fraberret <f.berretta02@gmail.com>
2024-04-10 11:25:12 +0200

    add products in data

--- src/data.js
+++ src/data.js
@@ -29,32 +29,147 @@ export const navVoices = [
 
 export const featuredProducts = [
     {
-        "name": 'Black Leather Jacket',
+        "name": 'Black Elegant Leather Jacket',
         "image": '/images/black_elegant_leather_jacket-400x520.jpg',
         "tags": 'Men, Jackets, Jeans',
+        "realPrice": 125,
+        "discountedPrice": false,
+        "featured": true,
+        "topRated": true,
+        "LatestReviews": true,
+        "onSale": true,
+        "newArrival": true,
+        "bestSeller": true
+    },
+    {
+        "name": 'Blue Leather Jacket',
+        "image": '/images/blue_leather_jacket-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 80,
+        "discountedPrice": 60,
+        "featured": false,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": true,
+        "newArrival": false,
+        "bestSeller": false
+    },
+    {
+        "name": 'Brown Dress Shoes',
+        "image": '/images/brown_dress_shoes-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 50,
+        "discountedPrice": 30,
+        "featured": false,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": true,
+        "newArrival": false,
+        "bestSeller": false
+    },
+    {
+        "name": 'Hipster Black top',
+        "image": '/images/hipster_black_top-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 57,
+        "discountedPrice": false,
+        "featured": true,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": false,
+        "newArrival": true,
+        "bestSeller": false
+    },
+    {
+        "name": 'Modern Love Tee',
+        "image": '/images/modern_love_tee-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 50,
+        "discountedPrice": 30,
+        "featured": false,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": false,
+        "newArrival": true,
+        "bestSeller": false
+    },
+    {
+        "name": 'Leather Gloves',
+        "image": '/images/leather_gloves-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 45,
+        "discountedPrice": false,
+        "featured": false,
+        "topRated": true,
+        "LatestReviews": true,
+        "onSale": false,
+        "newArrival": false,
+        "bestSeller": false
+    },
+    {
+        "name": 'Spring Printed Dress',
+        "image": '/images/spring_printed_dress-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
+        "realPrice": 47,
+        "discountedPrice": false,
+        "featured": false,
+        "topRated": true,
+        "LatestReviews": true,
+        "onSale": true,
+        "newArrival": true,
+        "bestSeller": false
+    },
+    {
+        "name": 'Black Leather Jacket',
+        "image": '/images/black_leather_jacket-400x520.jpg',
+        "tags": 'Men, Jackets, Jeans',
         "realPrice": 235,
-        "discountedPrice": 200
+        "discountedPrice": 200,
+        "featured": true,
+        "topRated": true,
+        "LatestReviews": true,
+        "onSale": true,
+        "newArrival": true,
+        "bestSeller": true
     },
     {
         "name": 'Black Leather Suit',
         "image": '/images/black_leather_suit-400x520.jpg',
         "tags": 'Men, Jackets',
         "realPrice": 176,
-        "discountedPrice": false
+        "discountedPrice": false,
+        "featured": true,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": false,
+        "newArrival": true,
+        "bestSeller": false
     },
     {
         "name": 'Blue Jacket & Stripe Tee',
         "image": '/images/blue_jacket_and_white_stripe_tee-400x520.jpg',
         "tags": 'Men, Jackets, Suits',
         "realPrice": 580,
-        "discountedPrice": false
+        "discountedPrice": false,
+        "featured": true,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": false,
+        "newArrival": false,
+        "bestSeller": false
     },
     {
         "name": 'Modern Black Leather Suit',
         "image": '/images/modern_black_leather_suit-400x520.jpg',
         "tags": 'Men, Jackets',
         "realPrice": 96,
-        "discountedPrice": false
+        "discountedPrice": false,
+        "featured": true,
+        "topRated": false,
+        "LatestReviews": false,
+        "onSale": false,
+        "newArrival": false,
+        "bestSeller": false
     },
 
 
